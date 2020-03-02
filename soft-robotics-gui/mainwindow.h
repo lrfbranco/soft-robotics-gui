3ad8062e60963a9fd369aaf00911bb573338bcd5
Luciano Branco <lrbranco@gmail.com>
2020-03-02 11:46:05 -0600

    All files commit

--- soft-robotics-gui/mainwindow.h
+++ soft-robotics-gui/mainwindow.h
@@ -1,21 +1,35 @@
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "qcustomplot.h"
+#include "axistag.h"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+
+private slots:
+  void timerSlot();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow *ui;
+  QCustomPlot *mPlot;
+  QPointer<QCPGraph> mGraph1;
+  QPointer<QCPGraph> mGraph2;
+  AxisTag *mTag1;
+  AxisTag *mTag2;
+  QTimer mDataTimer;
 };
+
+
 #endif // MAINWINDOW_H
--- soft-robotics-gui/soft-robotics-gui.pro
+++ soft-robotics-gui/soft-robotics-gui.pro
@@ -1,6 +1,6 @@
 QT       += core gui
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 
 CONFIG += c++11
 
@@ -16,11 +16,17 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    axistag.cpp \
+    backend.cpp \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    qcustomplot.cpp
 
 HEADERS += \
-    mainwindow.h
+    axistag.h \
+    backend.h \
+    mainwindow.h \
+    qcustomplot.h
 
 FORMS += \
     mainwindow.ui
