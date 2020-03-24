8b53490288f0e8600b948c85f6621ca5aa3cb8b0
Luciano Branco <lrbranco@gmail.com>
2020-03-24 09:10:19 -0500

    Added date label

--- soft-robotics-gui/mainwindow.cpp
+++ soft-robotics-gui/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDate>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setCurrentDate();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::setCurrentDate(){
+    QLocale::setDefault(QLocale("en_US"));
+    QDate currDate = QDate::currentDate();
+    ui->dateLabel->setText(QLocale().toString(currDate));
+}
--- soft-robotics-gui/mainwindow.h
+++ soft-robotics-gui/mainwindow.h
@@ -17,5 +17,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void setCurrentDate();
 };
 #endif // MAINWINDOW_H
