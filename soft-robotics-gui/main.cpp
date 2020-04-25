45a1364ad163ab7f5f41d1abc2b6641a8be8e998
Luciano Branco <lrbranco@gmail.com>
2020-04-25 15:44:38 -0500

    Theme fusion

--- soft-robotics-gui/main.cpp
+++ soft-robotics-gui/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QLocale::setDefault(QLocale("en_US"));
     QApplication a(argc, argv);
+    a.setStyle("fusion");
     MainWindow w;
     w.show();
     return a.exec();
